9ef97c8a665229ccf5576cd1304e3c9242c25659
Devin Bowers <djbowers@usfca.edu>
2018-02-01 10:26:17 -0800

    Added template to the deploy file

--- deploy.py
+++ deploy.py
@@ -1,3 +1,24 @@
 import numpy as np
 
+#!/usr/bin/env python
+
+import paramiko
+import time
+
+## Example Deploy Script
+## This file uses paramiko to login to a box. Note that this is a skeleton file and you will need to do a bunch to complete the assignment.
+
+print "Connecting to box"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('', username = '', key_filename = '' )
+
+
+ssh.exec_command("rm -rf pubcrawl; git clone git@bitbucket.org:kejnn/pubcrawl.git")
+print "Pull from BitBucket successful"
+time.sleep(10)
+print "Script fully executed ... exiting"
+ssh.close()
+## EOF ##
+
 
