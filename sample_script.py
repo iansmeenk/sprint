bde4adf95c3f298f471b43976e3549f0c7e704f3
Danielle Savage <danielle@Danielles-MacBook-Pro.local>
2018-02-09 16:18:03 -0800

    new script

---
+++ sample_script.py
@@ -0,0 +1,33 @@
+import os
+import json
+import sys
+
+prefix = sys.argv[1]
+#find_command = "find /srv/runme --name {0}*".format(prefix)
+#os.system(find_command)
+directory = '/srv/runme.'
+prefixed = [filename for filename in os.listdir(directory) if filename.startswith(prefix)]
+print prefixed
+
+output_file = open(prefix + '.txt', 'w')
+output_file.close()
+
+def process_file(file_name):
+    print 'trying to load'
+    opened_file = open(file_name)
+    for line in opened_file:
+        try:
+            loaded_json = json.loads(line)
+            print loaded_json
+            name = loaded_json["name"]
+            age = loaded_json["prop"]["age"]
+            if loaded_json["prop"]["age"] >= 0:
+                output_file = open(prefix + '.txt', 'a')
+                output_file.write(name+'\t'+str(age)+'\n')
+                output_file.close()
+        except:
+            print 'JSON not formatted correctly!'
+    opened_file.close()
+
+for f in prefixed:
+    process_file(f)
