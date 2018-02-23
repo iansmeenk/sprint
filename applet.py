33151647dcc82823f9cafcea94846d145b6e3c9e
Devin Bowers <djbowers@usfca.edu>
2018-02-23 15:50:48 -0800

    Added rotating logger

--- applet.py
+++ applet.py
@@ -1,15 +1,27 @@
 from flask import Flask, request
 from process_file import process_json
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 
 app = Flask(__name__)
+
+# Create rotating logs every 2 minutes
+logger = logging.getLogger("Roatating Logger")
+logger.setLevel(logging.INFO)
+handler = TimedRotatingFileHandler('json_processing_logs.txt', when='m', interval=2)
+logger.addHandler(handler)
 
 @app.route("/<prefix>", methods='POST')
 def process(prefix):
 
     content = request.get_json(silent=True)
-    content_processed  = process_json(content)
+    logger.info(content)
+    content_processed  = process_json(content, prefix) # Process and write to file
 
     return content_processed
+
+app.run()
+
 
 
