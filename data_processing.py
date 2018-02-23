47a555a338377b8f8bee67cfb6eff18e72208766
Spencer Stanley <spencerstanley@nana.local>
2018-02-23 15:38:05 -0800

    update prefix handling

--- data_processing.py
+++ data_processing.py
@@ -2,25 +2,24 @@ import os
 import json
 import sys
 
-prefix = sys.argv[1]
 #find_command = "find /srv/runme --name {0}*".format(prefix)
 #os.system(find_command)
 directory = '/srv/runme'
 
-def process_json(data):
+def process_json(data, prefix):
     """process json and write to <prefix>.txt
-    
-    prefix is determined when deploy is run
     
     params
     -----
     data: dictionary read from requests.get_json()
+    
+    prefix: directory where output should be written
     """
     try:
         name = data["name"]
         age = data["prop"]["age"]
         if data["prop"]["age"] >= 0:
-            output_file = open(directory + '/' + prefix + '.txt', 'a')
+            output_file = open(directory + '/' + prefix + '/proc.txt', 'a')
             output_file.write(name+'\t'+str(age)+'\n')
             output_file.close()
     except:
