81ef0834083d62d5e27940450ad4c519a2c6edda
Devin Bowers <djbowers@usfca.edu>
2018-02-25 12:23:32 -0800

    Trying wiht data now

--- applet.py
+++ applet.py
@@ -19,11 +19,11 @@ logger.addHandler(handler)
 @app.route("/", methods=['POST'])
 def process():
 
-    logger.info(request.form)
-    content = request.form
+    logger.info(request.data)
+    content = request.data
     print(content)
     process_json(content, prefix) # Process and write to file
-    return request.form
+    return request.data
 
 
 app.run(host='0.0.0.0', port=8080)
