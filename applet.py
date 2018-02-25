d2a4177201bafba81894c54f5aa00c2018143ce5
Devin Bowers <djbowers@usfca.edu>
2018-02-25 13:01:24 -0800

    Added danielles changes to data_processing

--- applet.py
+++ applet.py
@@ -13,16 +13,19 @@ app = Flask(__name__)
 # Create rotating logs every 2 minutes
 logger = logging.getLogger("Rotating Logger")
 logger.setLevel(logging.INFO)
-handler = TimedRotatingFileHandler('/home/ec2-user/srv/runme/Raw.txt', when='m', interval=2)
+#handler = TimedRotatingFileHandler('/srv/runme/'+prefix+'/Raw.txt', when='m', interval=2)
+handler = TimedRotatingFileHandler('/home/ec2-user/srv/runme/'+prefix+'/Raw.txt',
+                                   when='m', interval=2)
 logger.addHandler(handler)
 
+# Take a post request and process the body as json
 @app.route("/", methods=['POST'])
 def process():
 
     logger.info(request.data)
     content = request.data
     process_json(content, prefix) # Process and write to file
-    return "Request Processed"
+    return "Request Processed\n"
 
 
 app.run(host='0.0.0.0', port=8080)
--- data_processing.py
+++ data_processing.py
@@ -6,22 +6,36 @@ import sys
 #os.system(find_command)
 directory = '/home/ec2-user/srv/runme'
 
+
 def process_json(data, prefix):
     """process json and write to <prefix>.txt
-    
+
     params
     -----
     data: dictionary read from requests.get_json()
-    
+
     prefix: directory where output should be written
     """
+
+    def is_valid_age(age):
+        if age >= 0:
+            return True
+        else:
+            return False
+
+    def has_name(name):
+        if name != '':
+            return True
+        else:
+            return False
+
     try:
         data = json.loads(data)
         name = data["name"]
         age = data["prop"]["age"]
-        if data["prop"]["age"] >= 0:
+        if is_valid_age(age) and has_name(name):
             output_file = open(directory + '/' + prefix + '/proc.txt', 'a')
-            output_file.write(name+'\t'+str(age)+'\n')
+            output_file.write(name + '\t' + str(age) + '\n')
             output_file.close()
     except:
         print 'JSON not formatted correctly!'
